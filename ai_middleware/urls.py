5401954d59d77921ef1cd597b45a75fce6baf9d5
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-01-03 16:06:11 +0100

    Add markdown endpoints to API root

--- ai_middleware/urls.py
+++ ai_middleware/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
 from .views import (
     ConversationViewSet,
     MarkdownExportView,
@@ -11,7 +13,17 @@ from .views import (
     BreakOutViewSet
 )
 
-router = DefaultRouter()
+class CustomRouter(DefaultRouter):
+    def get_api_root_view(self, api_urls=None):
+        api_root_dict = {}
+        list_name = self.routes[0].name
+        for prefix, viewset, basename in self.registry:
+            api_root_dict[prefix] = list_name.format(basename=basename)
+        api_root_dict['markdown/export'] = 'markdown-export'
+        api_root_dict['markdown/import'] = 'markdown-import'
+        return self.APIRootView.as_view(api_root_dict=api_root_dict)
+
+router = CustomRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'clients', ClientViewSet)
 router.register(r'programmes', ProgrammeViewSet)
