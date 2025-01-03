4625d6017550ffc76b4997d34801c5bf5988dd7b
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-01-03 16:02:11 +0100

    Update root URLs

--- flowai/urls.py
+++ flowai/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ai_middleware.urls')),
-    path('', RedirectView.as_view(url='/api/', permanent=False)),
-    path('', include('social_django.urls', namespace='social')),  # Ajout des URLs d'authentification sociale
+    path('auth/', include('social_django.urls', namespace='social')),
 ]
