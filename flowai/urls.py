f2fd80e4d9a37ff58dd00a0eff08ff50291f0b9a
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-01-27 15:26:30 +0100

    Update URLs configuration to allow public access to API docs

--- flowai/urls.py
+++ flowai/urls.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.IsAuthenticated,),
+    permission_classes=[permissions.AllowAny],  # Changed this line
     url=f'https://127.0.0.1:8000' if settings.DEBUG else None
 )
 
@@ -26,7 +26,7 @@ urlpatterns = [
     path('auth/', include('social_django.urls', namespace='social')),
     
     # API Documentation
-    path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
