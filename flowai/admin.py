5d6c1eb38b2ce04f0d0e9c099568dd7673f0edc3
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-03-17 14:24:28 +0100

    Add LogEntry to admin interface for visibility

--- flowai/admin.py
+++ flowai/admin.py
@@ -1,10 +1,21 @@
-from django.contrib.admin import AdminSite
+from django.contrib import admin
 from django.contrib.admin.models import LogEntry
 
-class FlowAIAdminSite(AdminSite):
-    def index(self, request, extra_context=None):
-        if extra_context is None:
-            extra_context = {}
-        if 'log_entries' not in extra_context:
-            extra_context['log_entries'] = LogEntry.objects.select_related('content_type', 'user')[:10]
-        return super().index(request, extra_context)
+# Register the LogEntry model so it's visible in the admin
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag')
+    list_filter = ('action_time', 'user', 'content_type')
+    search_fields = ('object_repr', 'change_message')
+    readonly_fields = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag', 'change_message', 'object_id')
+    date_hierarchy = 'action_time'
+    
+    def has_add_permission(self, request):
+        return False
+        
+    def has_change_permission(self, request, obj=None):
+        return False
+        
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(LogEntry, LogEntryAdmin)
